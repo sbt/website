82a32ff8c85ea0a0edf477a20b9d0ee8a43fd7ad
Mark Harrah <dmharrah@gmail.com>
2013-01-05 15:12:36 -0500

    Latest 0.13.0-SNAPSHOT docs, including harrah/xsbt to sbt/sbt changes

--- 0.13.0-SNAPSHOT/docs/_static/searchtools.js
+++ 0.13.0-SNAPSHOT/docs/_static/searchtools.js
@@ -301,7 +301,7 @@ var Search = {
   },
 
   query : function(query) {
-    var stopwords = ["and","then","into","it","as","are","in","if","for","no","there","their","was","is","be","to","that","but","they","not","such","with","by","a","on","these","of","will","this","near","the","or","at"];
+    var stopwords = ["these","no","by","this","they","near","in","be","their","was","then","is","but","will","it","into","that","if","are","there","for","on","of","as","at","not","or","and","with","the","to","a","such"];
 
     // Stem the searchterms and add them to the correct list
     var stemmer = new Stemmer();
