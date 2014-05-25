3e6b1edb4aab82dd4ead395c69dbb1a02d0b05b7
Eugene Yokota <eed3si9n@gmail.com>
2014-05-24 22:28:12 -0400

    moving out settings to Docs

--- build.sbt
+++ build.sbt
@@ -1,9 +1,8 @@
 import com.typesafe.sbt.site.PamfletSupport
-import com.typesafe.sbt.git.GitRunner
+import Docs._
 
 lazy val Tutorial = config("tutorial")
 lazy val Ref = config("reference")
-lazy val targetSbtBinaryVersion = "0.13"
 
 site.settings
 
@@ -17,29 +16,4 @@ PamfletSupport.settings(Ref)
 
 site.addMappingsToSiteDir(mappings in Ref, s"""$targetSbtBinaryVersion/docs""")
 
-
-ghpages.settings
-
-git.remoteRepo := "git@github.com:sbt/website.git"
-
-def gitRemoveFiles(dir: File, files: List[File], git: GitRunner, s: TaskStreams): Unit = {
-  if(!files.isEmpty)
-    git(("rm" :: "-r" :: "-f" :: "--ignore-unmatch" :: files.map(_.getAbsolutePath)) :_*)(dir, s.log)
-  ()
-}
-
-def siteInclude(f: File) = true
-
-GhPagesKeys.synchLocal := {
-  val repo = GhPagesKeys.updatedRepository.value
-  val versioned = repo / targetSbtBinaryVersion
-  val git = GitKeys.gitRunner.value
-  val s = streams.value
-  gitRemoveFiles(repo, IO.listFiles(versioned).toList, git, s)
-  gitRemoveFiles(repo, (repo * "*.html").get.toList, git, s)
-  val mappings =  for {
-    (file, target) <- SiteKeys.siteMappings.value if siteInclude(file)
-  } yield (file, repo / target)
-  IO.copy(mappings)
-  repo
-}
+customGhPagesSettings // see project/Docs.scala
---
+++ project/Docs.scala
@@ -0,0 +1,66 @@
+import sbt._
+import Keys._
+import com.typesafe.sbt.git.GitRunner
+import com.typesafe.sbt.{SbtGhPages, SbtGit, SbtSite, site=>sbtsite}
+import SbtGhPages.{ghpages, GhPagesKeys => ghkeys}
+import SbtGit.{git, GitKeys}
+import SbtSite.SiteKeys
+
+object Docs {
+  // New documents will live under /x.y/ instead of /x.y.z/.
+  // Currently the following files needs to be manually updated:
+  // - src/nanoc/nanoc.yaml
+  // - src/reference/template.properties
+  // - src/tutorial/template.properties
+  lazy val targetSbtBinaryVersion = "0.13"
+  lazy val targetSbtFullVersion = "0.13.2"
+
+  def customGhPagesSettings: Seq[Setting[_]] = ghpages.settings ++ Seq(
+    git.remoteRepo := "git@github.com:sbt/website.git",
+    ghkeys.synchLocal := syncLocalImpl.value
+  )
+
+  // This task is responsible for updating the gh-pages branch on some temp dir.
+  // On the branch there are files that was generated in some other ways such as:
+  // - Scaladoc API doc
+  // - SXR hyperlinked source
+  // - older documentation
+  //
+  // This task's job is to call "git rm" on files and directories that this project owns
+  // and then copy over the newly generated files.
+  val syncLocalImpl = Def.task {
+    // sync the generated site
+    val repo = ghkeys.updatedRepository.value
+    val fullVersioned = repo / targetSbtFullVersion
+    val versioned = repo / targetSbtBinaryVersion
+    val git = GitKeys.gitRunner.value
+    val s = streams.value
+    gitRemoveFiles(repo, IO.listFiles(versioned).toList, git, s)
+    gitRemoveFiles(repo, (repo * "*.html").get.toList, git, s)
+    val mappings =  for {
+      (file, target) <- SiteKeys.siteMappings.value if siteInclude(file)
+    } yield (file, repo / target)
+    IO.copy(mappings)
+
+    // symlink API and SXR
+    symlink((versioned / "api").getAbsolutePath, fullVersioned / "api", s.log)
+    symlink((versioned / "sxr").getAbsolutePath, fullVersioned / "sxr", s.log)
+
+    repo
+  }
+
+  def gitRemoveFiles(dir: File, files: List[File], git: GitRunner, s: TaskStreams): Unit = {
+    if(!files.isEmpty)
+      git(("rm" :: "-r" :: "-f" :: "--ignore-unmatch" :: files.map(_.getAbsolutePath)) :_*)(dir, s.log)
+    ()
+  }
+
+  def siteInclude(f: File) = true
+
+  // TODO: platform independence/use symlink from Java 7
+  def symlink(path: String, file: File, log: Logger): Unit =
+    "ln" :: "-s" :: path :: file.getAbsolutePath :: Nil ! log match {
+      case 0 => ()
+      case code => sys.error("Could not create symbolic link '" + file.getAbsolutePath + "' with path " + path)
+    }
+}
