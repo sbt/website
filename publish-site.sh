e01224f73ad98ee9e6713cd3466df3b7187395e7
Eugene Yokota <eed3si9n@gmail.com>
2017-07-18 02:31:02 -0400

    switch to using just .travis.yml

--- publish-site.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "1.x" && "${TRAVIS_REPO_SLUG}" == "sbt/website" ]]; then
-  eval "$(ssh-agent -s)" #start the ssh agent
-  openssl aes-256-cbc -K $encrypted_e29f8a2ff2bf_key -iv $encrypted_e29f8a2ff2bf_iv -in deploy_rsa.enc -out deploy_rsa -d
-  chmod 600 deploy_rsa
-  ssh-add deploy_rsa
-  sbt ghpagesPushSite
-fi
