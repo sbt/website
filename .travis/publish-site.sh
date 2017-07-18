f3c6a71e7a8258ac09ed60254295bf83b71f245e
Eugene Yokota <eed3si9n@gmail.com>
2017-07-18 19:12:41 -0400

    openssl 1.0.2l

--- .travis/publish-site.sh
+++ .travis/publish-site.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "1.x" && "${TRAVIS_REPO_SLUG}" == "sbt/website" ]]; then
+  openssl version
   echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy_rsa\n" >> ~/.ssh/config
   openssl aes-256-cbc -pass "pass:$PRIV_KEY_SECRET" -in .travis/deploy_rsa.enc -out .travis/deploy_rsa -d -a
   chmod 600 .travis/deploy_rsa
