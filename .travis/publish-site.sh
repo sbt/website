5377db650f859a582ebfc167ff3146d29ae0a619
Eugene Yokota <eed3si9n@gmail.com>
2017-07-19 00:39:42 -0400

    target 0.13 branch

--- .travis/publish-site.sh
+++ .travis/publish-site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "1.x" && "${TRAVIS_REPO_SLUG}" == "sbt/website" ]]; then
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "0.13" && "${TRAVIS_REPO_SLUG}" == "sbt/website" ]]; then
   echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy_rsa\n" >> ~/.ssh/config
   openssl aes-256-cbc -K $encrypted_e29f8a2ff2bf_key -iv $encrypted_e29f8a2ff2bf_iv -in .travis/deploy_rsa.enc -out .travis/deploy_rsa -d
   chmod 600 .travis/deploy_rsa
