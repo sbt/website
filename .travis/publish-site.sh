62fc7820dc3e069766f9f3f2b227840fac5d18b5
Eugene Yokota <eed3si9n@gmail.com>
2017-07-18 20:08:27 -0400

    typo

--- .travis/publish-site.sh
+++ .travis/publish-site.sh
@@ -3,7 +3,7 @@
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "1.x" && "${TRAVIS_REPO_SLUG}" == "sbt/website" ]]; then
   openssl version
   echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy_rsa\n" >> ~/.ssh/config
-  openssl aes-256-cbc -K $encrypted_e29f8a2ff2bf_key -iv $encrypted_e29f8a2ff2bf_iv -in .travis/deploy_rsa.enc -out ./travis/deploy_rsa -d
+  openssl aes-256-cbc -K $encrypted_e29f8a2ff2bf_key -iv $encrypted_e29f8a2ff2bf_iv -in .travis/deploy_rsa.enc -out .travis/deploy_rsa -d
   chmod 600 .travis/deploy_rsa
   cp .travis/deploy_rsa ~/.ssh/
   sbt ghpagesPushSite
