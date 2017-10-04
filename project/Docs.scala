747b4885e73053ff4899aab2a1dd6a51f2cf4fe0
Eugene Yokota <eed3si9n@gmail.com>
2017-10-04 00:56:48 -0400

    beta site

--- project/Docs.scala
+++ project/Docs.scala
@@ -19,8 +19,8 @@ object Docs {
   // Currently the following files needs to be manually updated:
   // - src/nanoc/nanoc.yaml
   // - src/reference/template.properties
-  lazy val targetSbtBinaryVersion = "1.x"
-  lazy val targetSbtFullVersion = "1.0.0-RC2"
+  lazy val targetSbtBinaryVersion = "1.x-beta"
+  // lazy val targetSbtFullVersion = "1.0.0-RC2"
   lazy val siteEmail = settingKey[String]("")
 
   val isGenerateSiteMap = settingKey[Boolean]("generates site map or not")
@@ -248,7 +248,7 @@ object Docs {
   val syncLocalImpl = Def.task {
     // sync the generated site
     val repo = ghpagesUpdatedRepository.value
-    val fullVersioned = repo / targetSbtFullVersion
+    // val fullVersioned = repo / targetSbtFullVersion
     val versioned = repo / targetSbtBinaryVersion
     val git = GitKeys.gitRunner.value
     val s = streams.value
@@ -265,14 +265,14 @@ object Docs {
     // if (releaseLink.exists) releaseLink.delete
 
     gitRemoveFiles(repo, IO.listFiles(versioned).toList, git, s)
-    gitRemoveFiles(repo, (repo * "*.html").get.toList, git, s)
-    gitRemoveFiles(repo,
-                   List(
-                     repo / "assets" / "favicon.ico",
-                     repo / "assets" / "stylesheet.css",
-                   ),
-                   git,
-                   s)
+    // gitRemoveFiles(repo, (repo * "*.html").get.toList, git, s)
+    // gitRemoveFiles(repo,
+    //                List(
+    //                  repo / "assets" / "favicon.ico",
+    //                  repo / "assets" / "stylesheet.css",
+    //                ),
+    //                git,
+    //                s)
 
     val ms = for {
       (file, target) <- (mappings in makeSite).value if siteInclude(file)
