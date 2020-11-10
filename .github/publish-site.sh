dc892ccdd54e5a0c5d2d806d965842145b34e001
Eugene Yokota <eed3si9n@gmail.com>
2020-11-09 22:33:38 -0500

    Fix key path

--- .github/publish-site.sh
+++ .github/publish-site.sh
@@ -4,7 +4,7 @@ mkdir -p $HOME/.ssh/
 echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy_rsa\n" >> ~/.ssh/config
 
 gpg --quiet --batch --yes --decrypt --passphrase="$DEPLOY_KEY_PASSPHRASE" \
---output .github/deploy_rsa deploy_sbt_website_rsa.gpg
+--output .github/deploy_rsa .github/deploy_sbt_website_rsa.gpg
 chmod 600 .github/deploy_rsa
 cp .github/deploy_rsa $HOME/.ssh/
 sbt -v -Dsbt.website.detect_pdf ghpagesPushSite
