3cc74fdd36f9f1e2656d3ec39b278b1dde2a0521
Eugene Yokota <eed3si9n@gmail.com>
2020-11-09 21:34:49 -0500

    Fix deployment

---
+++ .github/publish-site.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+
+echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy_rsa\n" >> ~/.ssh/config
+
+gpg --quiet --batch --yes --decrypt --passphrase="$DEPLOY_KEY_PASSPHRASE" \
+--output .github/deploy_rsa deploy_sbt_website_rsa.gpg
+chmod 600 .github/deploy_rsa
+cp .github/deploy_rsa $HOME/.ssh/
+sbt -v -Dsbt.website.detect_pdf ghpagesPushSite
