5b9394b76caa4be7d6982f130942ba295889c903
xhudik <xhudik@gmail.com>
2021-04-11 21:31:39 +0200

    example eble to compile

--- src/sbt-test/ref/example-scalatest/changes/HelloSpec.scala
+++ src/sbt-test/ref/example-scalatest/changes/HelloSpec.scala
@@ -1,6 +1,6 @@
-import org.scalatest._
+import org.scalatest.funsuite._
 
-class HelloSpec extends FunSuite with DiagrammedAssertions {
+class HelloSpec extends AnyFunSuite {
   test("Hello should start with H") {
     // Hello, as opposed to hello
     assert("Hello".startsWith("H"))
