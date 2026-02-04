5ac1cf84aa2ca9b602229a46574dd2a62f3f62cc
Eugene Yokota <eed3si9n@gmail.com>
2026-02-04 01:31:13 -0500

    sbt 1.12.2

--- project/Docs.scala
+++ project/Docs.scala
@@ -12,7 +12,7 @@ import com.typesafe.sbt.site.SitePlugin
 import SitePlugin.autoImport._
 
 object Docs {
-  lazy val targetSbtFullVersion = "1.12.1"
+  lazy val targetSbtFullVersion = "1.12.2"
   lazy val sbtVersionForScalaDoc = "1.12.0"
   lazy val sbtWindowsBuild = targetSbtFullVersion
   lazy val scala3ExampleVersion = "3.7.2"
