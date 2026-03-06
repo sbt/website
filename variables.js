ee3f2f2cc8e0ebb5722aec760df1029834f15799
Eugene Yokota <eed3si9n@gmail.com>
2026-03-06 00:15:22 -0500

    sbt 1.12.5

--- variables.js
+++ variables.js
@@ -1,7 +1,8 @@
-const sbtVersion = "1.12.4";
+const sbtVersion = "1.12.5";
 const scala3ExampleVersion = "3.7.2";
 const releasesBase = "https://github.com/sbt/sbt/releases/download";
 const VersionList = [
+  { v: "1.12.5" },
   { v: "1.12.4" },
   { v: "1.12.3" },
   { v: "1.12.2" },
