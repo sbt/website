ad04cf5b0455bbc9fffc2c7c3586ef81e9985a96
Eugene Yokota <eed3si9n@gmail.com>
2026-07-13 09:28:31 -0400

    sbt 2.0.2

--- landing/_data/versions.js
+++ landing/_data/versions.js
@@ -1,7 +1,8 @@
-const sbtVersion = "2.0.1";
+const sbtVersion = "2.0.2";
 const scala3ExampleVersion = "3.8.4";
 const releasesBase = "https://github.com/sbt/sbt/releases/download";
 const VersionList2 = [
+  { v: "2.0.2" },
   { v: "2.0.1" },
   { v: "2.0.0" },
 ];
